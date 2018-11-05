b1715a98cdaa940fabc7f82b83fa84542a03ac2c
dwk715 <dwk715@gmail.com>
2018-11-06 07:58:55 +0800

    拆分模块，添加ASA5525-1.yml流量配置

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 # Date: 2018/11/5
 # Autor :  zlw dwk zly
 
-
-
 from apscheduler.schedulers.blocking import BlockingScheduler
 import sys
 sys.path.append('plugins')
@@ -13,13 +11,14 @@ from log import logmode
 
 log_ap = logmode('apscheduler').getlog()
 
+
 def job_ping():
     task.run('ping')
 
 
 def job_flow():
     task.run('flow')
-  
+
 
 def job_cup_memory():
     task.run('cpu_memory')
@@ -27,19 +26,25 @@ def job_cup_memory():
 
 def job_interface():
     task.run('interface')
-  
+
+
 scheduler = BlockingScheduler()
 scheduler.add_job(job_interface, 'cron', day_of_week='0-6', hour=8, minute=00)
-scheduler.add_job(job_cup_memory, 'cron', day_of_week='0-6', hour=10, minute=30)
+scheduler.add_job(
+    job_cup_memory, 'cron', day_of_week='0-6', hour=10, minute=30)
 scheduler.add_job(job_flow, 'cron', day_of_week='0-6', hour=10, minute=30)
 scheduler.add_job(job_flow, 'cron', day_of_week='0-6', hour=22, minute=20)
 scheduler.add_job(job_ping, 'cron', day_of_week='0-6', hour=8, minute=00)
+
+
 def main():
     try:
         # task.run('ping')
         task.run('flow')
         scheduler.start()
-    except (KeyboardInterrupt, SystemExit): 
+    except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
+
+
 if __name__ == '__main__':
     main()
