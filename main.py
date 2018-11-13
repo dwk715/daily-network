d272b60091c4664a505ae99de1f95e46b2d58146
dwk715 <dwk715@gmail.com>
2018-11-13 09:11:41 +0800

    修正ping的计划任务启动时间

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ scheduler.add_job(
     job_cup_memory, 'cron', day_of_week='0-6', hour=10, minute=30)
 scheduler.add_job(job_flow, 'cron', day_of_week='0-6', hour=10, minute=30)
 scheduler.add_job(job_flow, 'cron', day_of_week='0-6', hour=22, minute=20)
-scheduler.add_job(job_ping, 'cron', day_of_week='0-6', hour=10, minute=10)
+scheduler.add_job(job_ping, 'cron', day_of_week='0-6', hour=8, minute=30)
 '''
 主函数
 开启scheduler
